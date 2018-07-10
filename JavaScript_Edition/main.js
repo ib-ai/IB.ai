97839747ee643b69d49d553cfc820547a7701640
pants1 <dwarvenplague@gmail.com>
2018-07-11 00:34:25 +0800

    Functioning mod mail bot

--- JavaScript_Edition/main.js
+++ JavaScript_Edition/main.js
@@ -1,18 +1,23 @@
 const Discord = require('discord.js');
-//const Listeners = require('./listeners/listeners.js');
 const fs = require('fs');
 
 const client = new Discord.Client();
-var token = '';
+let token = '';
+let webHook = null;
 
 // Opens file reader and gets token from token.json file
 // If file does not exist, generates file and stops
 if (fs.existsSync('token.json')) {
-    var tokenJSONObj = JSON.parse(fs.readFileSync('token.json', 'utf8'));
+    let tokenJSONObj = JSON.parse(fs.readFileSync('token.json', 'utf8'));
     token = tokenJSONObj.token;
+    webHook = new Discord.WebhookClient(tokenJSONObj.webHookID, tokenJSONObj.webHookToken);
+
+    if(webHook == null){
+        return;
+    }
 
 }else {
-    fs.writeFileSync('token.json', '{\"token\":"TOKEN_HERE"}');
+    fs.writeFileSync('token.json', '{\"token\":"TOKEN_HERE",\"webHookToken\":\"WEBHOOK_TOKEN_HERE\",\"webHookID\":\"WEBHOOK_ID_HERE\"}');
     throw new Error('Token.json was not found and has been created for you.');
 }
 
@@ -20,7 +25,8 @@ if (fs.existsSync('token.json')) {
 
 // Bot 'ready' event
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ game: { name: 'dm to message mods <3' }, status: 'online' });
 });
 
 // Bot 'message' event
@@ -33,7 +39,13 @@ client.on('message', msg => {
     }
 
     if (msg.channel.type === 'dm') {
+        if(msg.content.length === 0){
+            return;
+        }
+
         console.log('DM from ' + msg.author.username + '#' + msg.author.discriminator + ': \"' + msg.content + '\"');
+        webHook.send('**User ' + msg.author.username + '#' + msg.author.discriminator + ' says:**' +
+            '\n' + msg.content);
     }
 });
 
