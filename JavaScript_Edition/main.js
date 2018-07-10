c58d20a03c5d82b7e5e237e320b5284924287498
pants1 <dwarvenplague@gmail.com>
2018-07-10 19:14:59 +0800

    JavaScript Edition start with modmail code

---
+++ JavaScript_Edition/main.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var token = '';
+
+// Opens file reader and gets token from token.json file
+// If file does not exist, generates file and stops
+var fs = require('fs');
+if (fs.existsSync('token.json')) {
+    var tokenJSONObj = JSON.parse(fs.readFileSync('token.json', 'utf8'));
+    token = tokenJSONObj.token;
+
+}else {
+    fs.writeFileSync('token.json', '{\"token\":"TOKEN_HERE"}');
+    throw new Error('Token.json was not found and has been created for you.');
+}
+
+// Declaring listeners
+
+// Bot 'ready' event
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Bot 'message' event
+// Hears DMs, Group DMs and Text-Channels
+client.on('message', msg => {
+
+    // Filter out bot messages
+    if (msg.author.bot) {
+        return;
+    }
+
+    if (msg.channel.type === 'dm') {
+        console.log('DM from ' + msg.author.username + '#' + msg.author.discriminator + ': ' + msg.content);
+    }
+});
+
+client.login(token);
