6ae67d253475f1be196beef27c74844045d63614
pants <dwarvenplague@gmail.com>
2018-07-16 22:45:03 +0800

    Minor structural changes, OOP beginning

--- JavaScript_Edition/main.js
+++ JavaScript_Edition/main.js
@@ -1,38 +1,62 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
-const client = new Discord.Client();
-let token = '';
+//TODO: move class declerations to seperate file?
+// Keep maing file for initialisation and startup^?
+class Bot {
+	constructor() {}
 
-// Opens file reader and gets token from token.json file
-// If file does not exist, generates file and stops
-if (fs.existsSync('token.json')) {
-    let tokenJSONObj = JSON.parse(fs.readFileSync('token.json', 'utf8'));
-    token = tokenJSONObj.token;
+	botLogin(token) {
+		client.login(token);
+	}
 
-}else {
-    fs.writeFileSync('token.json', '{\"token\":"TOKEN_HERE"}');
-    throw new Error('Token.json was not found and has been created for you.');
+	//TODO: Move find-token to utilities
+	findToken(path) {
+		// Opens file reader and gets token from token.json file
+		// If file does not exist, generates file and stops
+		if (fs.existsSync(path)) {
+			let tokenJSONObj = JSON.parse(fs.readFileSync('token.json', 'utf8'));
+			return tokenJSONObj.token;
+
+		} else {
+			fs.writeFileSync('token.json', '{\"token\":"TOKEN_HERE"}');
+			throw new Error('Token.json was not found and has been created for you.');
+		}
+	}
 }
 
-// Declaring listeners
-// *
-// *
-// *
+const client = new Discord.Client();
+let bot = new Bot();
+
+function startup(betaMode, pathToken) {
+	//TODO: Print splashscreen
+	//TODO: Startup database
+	//TODO: Initialise configuration from database
+	//TODO: Setup commands and listeners
+	//TODO: Setup utilities where needed
+	bot.botLogin(bot.findToken(pathToken));
+}
+
+//TODO: Startup variables from config
+startup(false, 'token.json');
+
+// * * * * * * * * * * * * * * * //
+// Listener Declerations
+// * * * * * * * * * * * * * * * //
+
+//TODO: Move listeners into seperate files
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setPresence({ game: { name: '3.0 | discord.gg/IBO' }, status: 'dnd' });
+	console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setPresence({
+		game: {
+			name: '3.0 | discord.gg/IBO'
+		},
+		status: 'dnd'
+	});
 });
 
 
 client.on('message', msg => {
-  // On message
+	// On message
 });
-
-// Bot startup
-// *
-// *
-// *
-
-client.login(token);
