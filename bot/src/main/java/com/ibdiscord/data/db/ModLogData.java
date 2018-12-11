e20fa67a51b7ccb3116e46fec4d1bc2e606c54e5
Ray Clark <raynichc@users.noreply.github.com>
2018-12-11 15:00:44 +0800

    Created ModLogData

---
+++ bot/src/main/java/com/ibdiscord/data/db/ModLogData.java
@@ -0,0 +1,17 @@
+package com.ibdiscord.data.db;
+
+import de.arraying.gravity.data.types.TypeValue;
+
+public class ModLogData extends TypeValue {
+
+    private String guildID;
+
+    public ModLogData(String guildID) {
+        this.guildID = guildID;
+    }
+
+    @Override
+    protected String getUniqueIdentifier() {
+        return "guild_" + this.guildID + "_modLogID ";
+    }
+}
