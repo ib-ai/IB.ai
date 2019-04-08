268669830f5d9a6d17267a3d4d4136117dc573cc
vardy <jarred.vardy@gmail.com>
2019-04-08 11:05:08 +0100

    Fixed ping to return correct date

--- api/routes/ping.js
+++ api/routes/ping.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.status(200).send(toString(new Date().getTime()));
+    res.status(200).send(toString(Date.now()));
 });
 
 module.exports = router;
