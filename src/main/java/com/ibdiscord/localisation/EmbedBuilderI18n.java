00541d4116553492abd5afc27e0427363d200235
Arraying <paul.huebner@googlemail.com>
2020-02-09 17:34:01 +0100

    Checkstyle fix: EmbedBuilderI18n

--- src/main/java/com/ibdiscord/localisation/EmbedBuilderI18n.java
+++ src/main/java/com/ibdiscord/localisation/EmbedBuilderI18n.java
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with IB.ai. If not, see http://www.gnu.org/licenses/.
  */
+
 package com.ibdiscord.localisation;
 
 import com.ibdiscord.command.CommandContext;
@@ -48,120 +49,245 @@ public final class EmbedBuilderI18n implements ILocalised {
         this.context = context;
     }
 
+    /**
+     * See parent.
+     * @param inline See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n addBlankField(boolean inline) {
         nest.addBlankField(inline);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param name See parent.
+     * @param value See parent.
+     * @param inline See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n addField(StringI18n name, StringI18n value, boolean inline) {
         nest.addField(i18n(name), i18n(value), inline);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param field See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n addField(MessageEmbed.Field field) {
         nest.addField(field);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param sequence See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n appendDescription(StringI18n sequence) {
         nest.appendDescription(i18n(sequence));
         return this;
     }
 
+    /**
+     * See parent.
+     * @return See parent.
+     */
     public MessageEmbed build() {
         return nest.build();
     }
 
+    /**
+     * See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n clear() {
         nest.clear();
         return this;
     }
 
+    /**
+     * See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n clearFields() {
         nest.clearFields();
         return this;
     }
 
+    /**
+     * See parent.
+     * @return See parent.
+     */
     public StringBuilder getDescriptionBuilder() {
         return nest.getDescriptionBuilder();
     }
 
+    /**
+     * See parent.
+     * @return See parent.
+     */
     public List<MessageEmbed.Field> getFields() {
         return nest.getFields();
     }
 
+    /**
+     * See parent.
+     * @return See parent.
+     */
     public boolean isEmpty() {
         return nest.isEmpty();
     }
 
+    /**
+     * See parent.
+     * @param type See parent.
+     * @return See parent.
+     */
     public boolean isValidLength(AccountType type) {
         return nest.isValidLength(type);
     }
 
+    /**
+     * See parent.
+     * @return See parent.
+     */
     public int length() {
         return nest.length();
     }
 
+    /**
+     * See parent.
+     * @param name See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setAuthor(StringI18n name) {
         nest.setAuthor(i18n(name));
         return this;
     }
 
+    /**
+     * See parent.
+     * @param name See parent.
+     * @param url See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setAuthor(StringI18n name, String url) {
         nest.setAuthor(i18n(name), url);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param name See parent.
+     * @param url See parent.
+     * @param iconUrl See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setAuthor(StringI18n name, String url, String iconUrl) {
         nest.setAuthor(i18n(name), url, iconUrl);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param color See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setColor(int color) { // If this wasn't 1:1 I would rename to setColour. Frick US spelling.
         nest.setColor(color);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param color See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setColor(Color color) {
         nest.setColor(color);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param description See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setDescription(StringI18n description) {
         nest.setDescription(i18n(description));
         return this;
     }
 
+    /**
+     * See parent.
+     * @param text See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setFooter(StringI18n text) {
         nest.setFooter(i18n(text));
         return this;
     }
 
+    /**
+     * See parent.
+     * @param text See parent.
+     * @param iconUrl See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setFooter(StringI18n text, String iconUrl) {
         nest.setFooter(i18n(text), iconUrl);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param url See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setImage(String url) {
         nest.setImage(url);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param url See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setThumbnail(String url) {
         nest.setThumbnail(url);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param temporal See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setTimestamp(TemporalAccessor temporal) {
         nest.setTimestamp(temporal);
         return this;
     }
 
+    /**
+     * See parent.
+     * @param title See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setTitle(StringI18n title) {
         nest.setTitle(i18n(title));
         return this;
     }
 
+    /**
+     * See parent.
+     * @param title See parent.
+     * @param url See parent.
+     * @return See parent.
+     */
     public EmbedBuilderI18n setTitle(StringI18n title, String url) {
         nest.setTitle(i18n(title), url);
         return this;
