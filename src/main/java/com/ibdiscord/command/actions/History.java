ec14c778532a00e1bd63ad01186483fc1837913c
Nathaneal Varghese <nathaneal.varghese@gihs.sa.edu.au>
2020-06-01 16:43:58 +0930

    Added history command to view past offences of user

---
+++ src/main/java/com/ibdiscord/command/actions/History.java
@@ -0,0 +1,92 @@
+/* Copyright 2020 Nathaneal Varghese
+ *
+ * This file is part of IB.ai.
+ *
+ * IB.ai is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IB.ai is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IB.ai. If not, see http://www.gnu.org/licenses/.
+ */
+
+package com.ibdiscord.command.actions;
+
+import com.ibdiscord.IBai;
+import com.ibdiscord.command.CommandAction;
+import com.ibdiscord.command.CommandContext;
+import com.ibdiscord.data.db.DataContainer;
+import com.ibdiscord.data.db.entries.punish.PunishmentData;
+import com.ibdiscord.data.db.entries.punish.PunishmentsData;
+import com.ibdiscord.punish.Punishment;
+import com.ibdiscord.punish.PunishmentHandler;
+import de.arraying.gravity.Gravity;
+import de.arraying.gravity.data.property.Property;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.ibdiscord.data.db.entries.punish.PunishmentData.MESSAGE;
+
+public final class History implements CommandAction {
+
+    /**
+     * Returns history of user.
+     * @param context The command context.
+     */
+    @Override
+    public void accept(CommandContext context) {
+        context.assertArguments(1, "error.generic_syntax_arg");
+
+        Guild guild = context.getGuild();
+        Gravity gravity = DataContainer.INSTANCE.getGravity();
+        PunishmentsData punishmentList = gravity.load(new PunishmentsData(guild.getId()));
+
+        Member member = context.getGuild().getMemberById(context.getArguments()[0]);
+
+        List<Long> caseIds = punishmentList.values().stream()
+                .filter(caseId -> Punishment.of(guild, caseId).getUserId().equals(context.getArguments()[0]))
+                .map(Property::asLong)
+                .collect(Collectors.toList());
+
+        IBai.INSTANCE.getLogger().info(caseIds.toString());
+
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle(String.format("History Of %s",
+                member == null ? context.getArguments()[0] : member.getUser().getAsTag()));
+        caseIds.forEach((caseId) -> {
+            Punishment punishment = Punishment.of(guild, caseId);
+            PunishmentData punishmentData = gravity.load(new PunishmentData(guild.getId(), caseId));
+            PunishmentHandler punishmentHandler = new PunishmentHandler(guild, punishment);
+
+            TextChannel channel = punishmentHandler.getLogChannel();
+            if(channel == null) {
+                context.replyI18n("error.reason_logging");
+                return;
+            }
+
+            Message message  = channel
+                    .retrieveMessageById(punishmentData.get(MESSAGE).defaulting(0L).asLong())
+                    .complete();
+
+            embedBuilder.addField(String.format("Case %s (%s) - By %s", caseId,
+                    message.getTimeCreated().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)),
+                    punishment.getStaffDisplay()),
+                    punishment.getReason(), false);
+        });
+        context.replyEmbed(embedBuilder.build());
+    }
+}
