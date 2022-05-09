729ff5c5201aa30dcc96dd1e0f19a486ef314466
Ray Clark <raynichc@users.noreply.github.com>
2022-05-09 12:24:17 +0800

    Fixed case number again

--- src/main/java/com/ibdiscord/punish/PunishmentHandler.java
+++ src/main/java/com/ibdiscord/punish/PunishmentHandler.java
@@ -23,7 +23,6 @@ import com.ibdiscord.data.db.entries.GuildData;
 import com.ibdiscord.data.db.entries.punish.PunishmentData;
 import com.ibdiscord.data.db.entries.punish.PunishmentsData;
 import de.arraying.gravity.Gravity;
-import java.util.Collections;
 import lombok.AllArgsConstructor;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -39,7 +38,7 @@ public final class PunishmentHandler {
      */
     public void onPunish() {
         Gravity gravity = DataContainer.INSTANCE.getGravity();
-        long caseNumber = Collections.max(gravity.load(new PunishmentsData(guild.getId())).values()) + 1;
+        long caseNumber = gravity.load(new PunishmentsData(guild.getId())).values().stream().map(property -> property.asLong()).max(Long::compare).get() + 1;
         punishment.dump(guild, caseNumber);
         TextChannel channel = getLogChannel(GuildData.MODLOGS);
         if(channel == null) {
